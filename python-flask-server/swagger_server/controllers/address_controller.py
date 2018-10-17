67cdd4e33d72d83651a6e691772ecff20a5a890b
MaesterPei <maesterpei@gmail.com>
2018-10-17 16:12:00 -0600

    Finished Google service and changed it to primary

--- python-flask-server/swagger_server/controllers/address_controller.py
+++ python-flask-server/swagger_server/controllers/address_controller.py
@@ -8,22 +8,63 @@ import urllib
 import json
 import re
 
-SERVICE_PROVIDERS = ['here', 'google']
+SERVICE_PROVIDERS = ['google', 'here']
+GOOGLE_SERVICE = 'google'
+HERE_SERVICE= 'here'
 PRIMARY_SERVICE = SERVICE_PROVIDERS[0]
 SECONDARY_SERVICE = SERVICE_PROVIDERS[1]
 
 def url2Request(service=PRIMARY_SERVICE, addr=""):
-    if service == PRIMARY_SERVICE:
+    urlBase = 'localhost'
+    queryStr = ''
+    if service == HERE_SERVICE:
         urlBase = 'https://geocoder.api.here.com/6.2/geocode.json'
         query = {}
         query['app_id'] = 'ontaBB1aZtjPd7dgm8E1'
         query['app_code'] = 'iMhp5B38Yq23C1eYVUm5og'
         query['locationattributes'] = 'none'
         query['searchtext'] = addr
-        print((key + '=' + query.get(key) for key in query))
         queryStr = '&'.join((key + '=' + query.get(key) for key in query))
-        return '{0}?{1}'.format(urlBase, queryStr)
-        
+    elif service == GOOGLE_SERVICE:
+        query = {}
+        urlBase = 'https://maps.googleapis.com/maps/api/geocode/json'
+        query['key'] = 'AIzaSyDKw7bn1fQUoqqAaBF6teXScWPxl8ByIkk'
+        query['address'] = addr
+        queryStr = '&'.join((key + '=' + query.get(key) for key in query))
+    return '{0}?{1}'.format(urlBase, queryStr)
+
+
+def serviceResultTranslate(service, result):
+    returnJson = {}
+    selectedServiceUnavaliable = False
+    print(result)
+    if service == GOOGLE_SERVICE:
+        """
+        Google service only returns one most possible location
+        """
+        if not result or not result.get('results'):
+            selectedServiceUnavaliable = True
+            returnJson = {'Failed': []}
+        else:
+            coordinate = result.get('results')[0].get('geometry').get('location')
+            returnJson = {'Success': {'PossibleLocations': [{'lat': coordinate.get('lat'), 'lng': coordinate.get('lng')}]}}
+    elif service == HERE_SERVICE:
+        """
+        Here service does the fuzzy search with address, we can get multiple possible locations
+        """
+        if not result or not result.get('Response'):
+            selectedServiceUnavaliable = True
+            returnJson = {'Failed': []}
+        else:
+            if (len(result.get('Response').get('View')) > 0 and 
+            len(result.get('Response').get('View')[0].get('Result')) > 0):
+                coordinates = [p.get('Location').get('NavigationPosition')[0] for p in result.get('Response').get('View')[0].get('Result')]
+                coordinates = [{'lat': c.get('Latitude'), 'lng': c.get('Longitude')} for c in coordinates]
+                returnJson = {'Success': {'PossibleLocations': coordinates}}
+            else:
+                returnJson = {'Success': {'PossibleLocations': []}}
+    return selectedServiceUnavaliable, returnJson
+
 def get_lat_lng_with_addr(addr=None):  # noqa: E501
     """get latitude and longitude of an address
 
@@ -35,23 +76,21 @@ def get_lat_lng_with_addr(addr=None):  # noqa: E501
     :rtype: ApiResponse
     """
     addr = '+'.join(re.findall(r"[\w',]+", addr))
-    _url2Request = url2Request(PRIMARY_SERVICE, addr)
-    print(_url2Request)
     primaryServiceUnavaliable = False
     returnJson = {}
     try:
+        _url2Request = url2Request(PRIMARY_SERVICE, addr)
         with urllib.request.urlopen(_url2Request) as r:
             result = json.load(r)
-            if not result or not result.get('Response'):
-                primaryServiceUnavaliable = True
-            else:
-                if (len(result.get('Response').get('View')) > 0 and 
-                len(result.get('Response').get('View')[0].get('Result')) > 0):
-                    coordinates = [p.get('Location').get('NavigationPosition')[0] for p in result.get('Response').get('View')[0].get('Result')]
-                    returnJson = {'PossibleLocations': coordinates}
+            primaryServiceUnavaliable, returnJson = serviceResultTranslate(PRIMARY_SERVICE, result)
     except:
         primaryServiceUnavaliable = True
     if primaryServiceUnavaliable:
-        pass        
+        _url2Request = url2Request(SECONDARY_SERVICE, addr)
+        try:
+            with urllib.request.urlopen(_url2Request) as r:
+                result = json.load(r)
+                primaryServiceUnavaliable, returnJson = serviceResultTranslate(SECONDARY_SERVICE, result)
+        except:
+            primaryServiceUnavaliable = True
     return returnJson
-
